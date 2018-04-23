9472264ce6ccfd3042878aa46b6037782747640e
mankirankaur <mankirankaur92@gmail.com>
2018-04-23 18:52:16 -0400

    Added three view Controllers

---
+++ MAPD724-W2018-Lesson13/View Controllers/StartViewController.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
+
+}
